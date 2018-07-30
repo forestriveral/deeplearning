8cc64c59d526be6c68ec3eb9be52640181e3b4ca
forestriveral <forestriveral@gmail.com>
2018-07-31 00:17:45 +0800

    basic_1 practice commit

---
+++ basic_1.py
@@ -0,0 +1,90 @@
+
+
+# age = 23
+# message = "Happy " + str(age) + "rd Birthday"
+# print(message)
+# num_1 = 123
+# num_2 = 321
+# print(num_1 + num_2)
+# import this
+
+# motorcycle = ['honda', 'yamaha', 'suzuki']
+# print(motorcycle)
+# del motorcycle[0]
+# print(motorcycle)
+# del motorcycle[1]
+# print(motorcycle)
+# popped_motorcycle = motorcycle.pop()
+# print(motorcycle)
+# print(popped_motorcycle)
+# lasted_owned = motorcycle.pop()
+# motorcycle.remove('honda')
+# print(motorcycle)
+# motorcycle.insert(2, 'ducati')
+# print(motorcycle)
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort(reverse = True)
+# print(cars)
+# print(sorted(cars,reverse = True))
+# print(cars)
+# print(len(cars))
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(magician.title() + ","
+#                              "that was a great trick!")
+#     print("I can't wait to see your next trick, " +
+#           magician.title() + ".\n")
+# print("Thank you! Everyone!")
+
+# for value in range(1, 5):
+#     print(value)
+# numbers = list(range(1, 20, 3))
+# print(numbers)
+# squares = []
+# for value in range(1, 11):
+#     squares.append(value**2)
+#     print(squares)
+# print(squares)
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+# players = ['charles', 'martina', 'michael',
+#            'florence', 'eli']
+# print(players[0:3])
+# print(players[-3:])
+# athletes = players
+# players.append('john')
+# athletes.append('mike')
+# print(players)
+# print(athletes)
+
+# dimensions = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# print((22 >= 21) and (18 >= 21))
+requested_toppings = ["mushroom", "green peppers"]
+if requested_toppings:
+    for requested_topping in  requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+
+
+
+
+
+
+
